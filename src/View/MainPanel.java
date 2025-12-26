fe340f7d0d69dce1e0ea6758efdff1c8482df61f
Alexander Rankov <alexanderrankov@gmail.com>
2025-12-26 09:18:25 -0500

    I reorganized my code so that everything is well placed and I made it so that the textfield is on top of the main buttons

--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -12,9 +12,11 @@ public class MainPanel extends JPanel implements ActionListener {
     public final int MAX_BOUTTONS = 10;
     public final int MAX_COLUMNS = 3;
 
-    JPanel mainPanel;
+    JPanel mainPanel = new JPanel();
 
     JPanel textFieldPanel;
+
+    JPanel buttonPanel;
 
     JButton buttons;
 
@@ -23,61 +25,57 @@ public class MainPanel extends JPanel implements ActionListener {
     public MainPanel()
     {
         panelLayout();
-        textFieldCreation();
-        buttonCreation();
-        panelCreation();
+        createMainPanel();
     }
 
     public void panelLayout()
     {
-        this.setLayout(new GridLayout(0, MAX_COLUMNS));
-
         mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(0, MAX_COLUMNS));
+
         mainPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 4));
         mainPanel.setPreferredSize(new Dimension(500, 200));
     }
 
-    public void textFieldCreation()
-    {
-        mainTextField = new JTextField();
-        mainTextField.setPreferredSize(new Dimension(100, 30));
-        mainTextField.setEditable(false);
-        textFieldPanel = new JPanel();
+    private void createMainPanel() {
+        mainPanel = new JPanel(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 4));
+        mainPanel.setPreferredSize(new Dimension(500, 300));
+
+        createTextFieldPanel();
+        createButtonPanel();
+
+        mainPanel.add(textFieldPanel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        add(mainPanel);
     }
 
-    public void buttonCreation()
-    {
-        buttons = new JButton();
 
-        for(int i = 0; i < MAX_BOUTTONS; i++)
-        {
-            buttons = new JButton(String.valueOf(i));
-            buttons.addActionListener(this);
-            buttons.setPreferredSize(new Dimension(100, 100));
-            buttons.setBackground(ColorPalette.MAIN_COLOR);
-            buttons.setForeground(ColorPalette.TEXT_COLOR);
-            buttons.setBorderPainted(false);
+    private void createTextFieldPanel() {
+        mainTextField = new JTextField();
+        mainTextField.setEditable(false);
 
-            add(buttons);
+        textFieldPanel = new JPanel(new BorderLayout());
+        textFieldPanel.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
+        textFieldPanel.add(mainTextField, BorderLayout.CENTER);
+    }
 
-            mainPanel.add(buttons);
+    private void createButtonPanel() {
+        buttonPanel = new JPanel(new GridLayout(0, MAX_COLUMNS, 5, 5));
+
+        for (int i = 0; i < MAX_BOUTTONS; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            button.addActionListener(this);
+            button.setBackground(ColorPalette.MAIN_COLOR);
+            button.setForeground(ColorPalette.TEXT_COLOR);
+            button.setBorderPainted(false);
+
+            buttonPanel.add(button);
         }
     }
 
-    public void panelCreation()
-    {
-        mainPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        textFieldPanel.add(mainTextField);
-        textFieldPanel.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
-        textFieldPanel.setPreferredSize(new Dimension(appDimension.width, 100));
-
-        mainPanel.add(textFieldPanel);
-
-
-        add(mainPanel, BorderLayout.CENTER);
-
-    }
 
     @Override
     public void actionPerformed(ActionEvent event)
