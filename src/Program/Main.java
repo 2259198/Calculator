ca69c71d40b2813de56abcb51890642bd4c63c75
Alexander Rankov <alexanderrankov@gmail.com>
2025-12-26 08:13:04 -0500

    I'm creating the main files for the project. I'm using the MVC pattern.

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,9 +1,9 @@
-
+package Program;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        //SwingUtilities.invokeLater();
 
     }
 }
