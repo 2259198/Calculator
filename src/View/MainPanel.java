3be885720c259d2ef4595a8a25aa5361861c64f7
Alexander Rankov <alexanderrankov@gmail.com>
2025-12-26 08:39:11 -0500

    I created the main panel and added all of the buttons inside the panel.

--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -1,6 +1,7 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,7 +10,7 @@ public class MainPanel extends JPanel implements ActionListener {
     public final int MAX_BOUTTONS = 10;
 
     JPanel mainPanel;
-    JButton[] buttons;
+    JButton buttons;
 
     public MainPanel()
     {
@@ -19,19 +20,27 @@ public class MainPanel extends JPanel implements ActionListener {
 
     public void panelLayout()
     {
-
+        mainPanel = new JPanel();
+        this.setLayout(new GridLayout(0, 3));
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 4));
+        mainPanel.setPreferredSize(new Dimension(500, 200));
     }
 
     public void configurationOfPanel()
     {
-        buttons = new JButton[MAX_BOUTTONS];
+        buttons = new JButton();
 
-        for(int i = 1; i < MAX_BOUTTONS; i++)
+        for(int i = 0; i < MAX_BOUTTONS; i++)
         {
-            buttons[i] = new JButton(String.valueOf(i));
-            buttons[i].addActionListener(this);
-            add(buttons[i]);
+            buttons = new JButton(String.valueOf(i));
+            buttons.addActionListener(this);
+            buttons.setPreferredSize(new Dimension(130, 30));
+            add(buttons);
+
+            mainPanel.add(buttons);
         }
+
+        add(mainPanel, BorderLayout.CENTER);
     }
 
     @Override
